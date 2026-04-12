3a189ee192f5b600ec24f01d5169e898441ee7a6
damian <macmacroll@gmail.com>
2026-04-12 20:03:10 +0200

    refactor: remove payment state machine service and add common DTOs

--- src/main/java/com/example/payments/payment/domain/event/PaymentDomainEvent.java
+++
@@ -1,8 +0,0 @@
-package com.example.payments.payment.domain.event;
-
-import org.jmolecules.event.annotation.DomainEvent;
-
-@DomainEvent
-public interface PaymentDomainEvent {
-    Long getPaymentId();
-}
