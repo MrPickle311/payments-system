e7d9569e746f1994f3459f7c7b9f2e502b384d38
damian <macmacroll@gmail.com>
2026-07-24 11:10:31 +0200

    Fixed formatting

--- sanctions-service/src/main/java/com/example/sanctions/SanctionsApplication.java
+++ sanctions-service/src/main/java/com/example/sanctions/SanctionsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SanctionsApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(SanctionsApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SanctionsApplication.class, args);
+    }
 }
