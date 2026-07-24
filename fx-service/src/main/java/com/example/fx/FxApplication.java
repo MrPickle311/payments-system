1c978fc891c624f75bd7e496cb50867e0997660a
damian <macmacroll@gmail.com>
2026-07-24 11:43:51 +0200

    Fixed formatting

--- fx-service/src/main/java/com/example/fx/FxApplication.java
+++ fx-service/src/main/java/com/example/fx/FxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FxApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(FxApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(FxApplication.class, args);
+    }
 }
