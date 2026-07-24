896697b40ab392a5ed3f1159368b02274e1461f5
damian <macmacroll@gmail.com>
2026-07-24 11:49:52 +0200

    Fixed formatting

--- authorization-service/src/main/java/com/example/authorization/AuthorizationGrpcService.java
+++ authorization-service/src/main/java/com/example/authorization/AuthorizationGrpcService.java
@@ -1,40 +1,40 @@
 package com.example.authorization;
 
-import com.example.payments.authorization.grpc.AuthorizationServiceGrpc;
 import com.example.payments.authorization.grpc.AuthorizationRequest;
 import com.example.payments.authorization.grpc.AuthorizationResponse;
+import com.example.payments.authorization.grpc.AuthorizationServiceGrpc;
 import io.grpc.stub.StreamObserver;
-import net.devh.boot.grpc.server.service.GrpcService;
 import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 @Slf4j
 @GrpcService
-public class AuthorizationGrpcService
-    extends AuthorizationServiceGrpc.AuthorizationServiceImplBase {
+public class AuthorizationGrpcService extends AuthorizationServiceGrpc.AuthorizationServiceImplBase {
 
-  @Override
-  public void authorize(AuthorizationRequest request,
-      StreamObserver<AuthorizationResponse> responseObserver) {
-    log.info("Simulating authorization for payment {}", request.getPaymentId());
-    try {
-      Thread.sleep(200);
-    } catch (InterruptedException _) {
-      Thread.currentThread().interrupt();
+    @Override
+    public void authorize(AuthorizationRequest request, StreamObserver<AuthorizationResponse> responseObserver) {
+        log.info("Simulating authorization for payment {}", request.getPaymentId());
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        AuthorizationResponse response =
+                AuthorizationResponse.newBuilder().setSuccess(true).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
-    AuthorizationResponse response = AuthorizationResponse.newBuilder().setSuccess(true).build();
+    @Override
+    public void reverseAuthorization(
+            AuthorizationRequest request, StreamObserver<AuthorizationResponse> responseObserver) {
+        log.info("Reversing authorization for payment {}", request.getPaymentId());
+        AuthorizationResponse response =
+                AuthorizationResponse.newBuilder().setSuccess(true).build();
 
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
-  }
-
-  @Override
-  public void reverseAuthorization(AuthorizationRequest request,
-      StreamObserver<AuthorizationResponse> responseObserver) {
-    log.info("Reversing authorization for payment {}", request.getPaymentId());
-    AuthorizationResponse response = AuthorizationResponse.newBuilder().setSuccess(true).build();
-
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
-  }
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
