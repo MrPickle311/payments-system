297e2729a4ca425e9bb9f01421d74e6166ddf8a0
damian <macmacroll@gmail.com>
2026-07-28 22:38:47 +0200

    updated commons

---
+++ payments-common/src/main/java/com/example/payments/common/sharedkernel/outbox/OutboxRepositoryProxy.java
@@ -0,0 +1,25 @@
+package com.example.payments.common.sharedkernel.outbox;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OutboxRepositoryProxy {
+
+    private final OutboxRepository outboxRepository;
+
+    @Retryable(
+            retryFor = {DataAccessException.class, Exception.class},
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 100, multiplier = 2.0, maxDelay = 2000))
+    public OutboxEventEntity save(OutboxEventEntity entity) {
+        log.debug("[OutboxProxy] Saving outbox event aggregateId={} eventType={}", entity.getAggregateId(), entity.getEventType());
+        return outboxRepository.save(entity);
+    }
+}
